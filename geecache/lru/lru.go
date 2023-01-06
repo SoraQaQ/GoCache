a40cbbb7e819cda6d6ce5da35d8f77aada00c3ed
Sora <1093164900@qq.com>
2023-01-06 07:44:44 -0800

    lru + Single machine concurrent cache + HttpServer

---
+++ geecache/lru/lru.go
@@ -0,0 +1,78 @@
+package lru
+
+import "container/list"
+
+type Cache struct {
+	maxBytes int64
+	//当前已使用内存
+	nbytes int64
+	ll     *list.List
+	cache  map[string]*list.Element
+	//可选的，并在清楚条目时候执行
+	OnEvicted func(key string, value Value)
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+type Value interface {
+	Len() int
+}
+
+func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		//interface 类型转换
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+func (c *Cache) RemoveOldSet() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		//当前可使用的内存
+		// 删除该数据key+value的值
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		//更新新的nbytes 老的值并没有被减去 只是被新的value覆盖了
+		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		//更新新值
+		c.nbytes += int64(len(key)) + int64(value.Len())
+	}
+	for c.maxBytes != 0 && c.maxBytes < c.nbytes {
+		c.RemoveOldSet()
+	}
+}
+
+func (c *Cache) Len() int {
+	return c.ll.Len()
+}
